49ec6d97afc5414b2b11ce672fed5a1828567e1e
shilei <shilei.007@163.com>
2024-12-09 21:22:53 +0800

    111

---
+++ src/main/java/com/yonyou/controller/TcysController.java
@@ -0,0 +1,12 @@
+package com.yonyou.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "剔除因素管理",description = "")
+@RestController
+public class TcysController {
+
+
+
+}
